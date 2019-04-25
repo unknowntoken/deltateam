ac64464681ddbc6bd078aa8e2d37b339e8f2b894
Kevin Dias <kevinsilveiradias@gmail.com>
2019-04-25 18:17:40 +0000

    removed server

--- server/src/main/java/org/academiadecodigo/tropadelete/commands/NicknameCommand.java
+++
@@ -1,7 +0,0 @@
-package org.academiadecodigo.tropadelete.commands;
-
-public class NicknameCommand implements CommandHandler {
-    public void handle() {
-
-    }
-}
--- server/src/main/java/org/academiadecodigo/tropadelete/commands/WhisperCommand.java
+++
@@ -1,7 +0,0 @@
-package org.academiadecodigo.tropadelete.commands;
-
-public class WhisperCommand implements CommandHandler {
-    public void handle() {
-
-    }
-}
