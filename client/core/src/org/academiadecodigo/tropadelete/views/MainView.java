f313dac2da88b043bcd663147d9c956b3a8f355d
Marco <marco.pereira90@gmail.com>
2019-04-25 17:48:41 +0000

    merge

--- client/core/src/org/academiadecodigo/tropadelete/views/MainView.java
+++ client/core/src/org/academiadecodigo/tropadelete/views/MainView.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
