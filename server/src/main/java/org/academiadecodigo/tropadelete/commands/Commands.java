0c2a020598a294644818c17c2afd887bc36e2f5b
Kevin Dias <kevinsilveiradias@gmail.com>
2019-04-25 13:11:45 +0000

    testing methodolody

--- server/src/main/java/org/academiadecodigo/tropadelete/commands/Commands.java
+++ server/src/main/java/org/academiadecodigo/tropadelete/commands/Commands.java
@@ -1,4 +1,14 @@
 package org.academiadecodigo.tropadelete.commands;
 
 public enum Commands {
+
+    server.sendComand (boradcast, "message");
+}
+
+
+
+public void handleIncommingMessage (Type type, Message message){
+
+    mainTextBox.addMessage (message);
+
 }
