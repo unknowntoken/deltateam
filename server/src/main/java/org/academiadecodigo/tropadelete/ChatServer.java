c568464d935091082afb6d9e4b840cb38751d4e8
Kevin Dias <kevinsilveiradias@gmail.com>
2019-04-25 11:58:13 +0000

    client directory

---
+++ server/src/main/java/org/academiadecodigo/tropadelete/ChatServer.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.tropadelete;
+
+public class ChatServer {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ server/src/main/java/org/academiadecodigo/tropadelete/commands/Commands.java
@@ -0,0 +1,4 @@
+package org.academiadecodigo.tropadelete.commands;
+
+public enum Commands {
+}
