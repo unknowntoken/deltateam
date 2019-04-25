146225e1fa257df0353451c600724dff334d2ead
Diogo Silveira <diogotostesilveira@gmail.com>
2019-04-25 16:00:38 +0000

    Elements placed in the respective position

--- client/core/src/org/academiadecodigo/tropadelete/views/MainView.java
+++ client/core/src/org/academiadecodigo/tropadelete/views/MainView.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.Widget;
 
 import java.awt.*;
 
@@ -19,6 +20,7 @@ public class MainView extends ApplicationAdapter implements InputProcessor {
     private Stage stage;
 
     private ShapeRenderer border;
+    private ShapeRenderer messageFieldBackground;
 
     private TextField inputMessage;
     private TextArea message_field;
@@ -33,27 +35,28 @@ public class MainView extends ApplicationAdapter implements InputProcessor {
 
     @Override
     public void create() {
+
         stage = new Stage();
 
+        TextField.TextFieldStyle messageStyle = new TextField.TextFieldStyle();
+        messageStyle.fontColor = Color.BLACK;
+        messageStyle.font = new BitmapFont();
 
-        TextField.TextFieldStyle style = new TextField.TextFieldStyle();
-        style.fontColor = Color.BLACK;
-        style.font = new BitmapFont();
+        TextField.TextFieldStyle inputStyle = new TextField.TextFieldStyle();
+        inputStyle.fontColor = Color.BLACK;
+        inputStyle.font = new BitmapFont();
 
-        message_field = new TextArea("", style);
-        message_field.setWidth(585);
+        message_field = new TextArea("", messageStyle);
+        message_field.setWidth(555);
         message_field.setHeight(400);
-        message_field.setX(10);
+        message_field.setX(20);
         message_field.setY(100);
+        message_field.toFront();
 
         border = new ShapeRenderer();
-        border.begin(ShapeRenderer.ShapeType.Filled);
-        border.setColor(Color.BLACK);
-        border.rect(message_field.getX()-1,message_field.getY()-1,message_field.getWidth()+1,message_field.getHeight()+1);
-        border.end();
+        messageFieldBackground = new ShapeRenderer();
 
-
-        inputMessage = new TextField("", style);
+        inputMessage = new TextField("", inputStyle);
         inputMessage.setPosition(5, 20);
         inputMessage.setSize(585, 15);
 
@@ -69,8 +72,25 @@ public class MainView extends ApplicationAdapter implements InputProcessor {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        borderAndBackground(message_field);
+        borderAndBackground(inputMessage);
+
         stage.draw();
         stage.act();
+    }
+
+    private void borderAndBackground(Widget element) {
+
+        border.begin(ShapeRenderer.ShapeType.Filled);
+        border.setColor(Color.BLACK);
+        border.rect(element.getX()-5,element.getY()-5,element.getWidth()+10,element.getHeight()+10);
+        border.end();
+
+        border.begin(ShapeRenderer.ShapeType.Filled);
+        border.setColor(Color.WHITE);
+        border.rect(element.getX(),element.getY(),element.getWidth(),element.getHeight());
+        border.end();
+
     }
 
     @Override
