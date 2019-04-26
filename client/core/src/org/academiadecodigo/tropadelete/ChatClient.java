a2937c4040130fc4d519f7dcdc63981e5ff393c3
Jose Moreira <gt96jom@gmail.com>
2019-04-26 01:47:45 +0000

    registerView Implemented

--- client/core/src/org/academiadecodigo/tropadelete/ChatClient.java
+++ client/core/src/org/academiadecodigo/tropadelete/ChatClient.java
@@ -6,33 +6,36 @@ import com.badlogic.gdx.InputProcessor;
 import org.academiadecodigo.tropadelete.networking.ConnectionHandler;
 import org.academiadecodigo.tropadelete.views.LoginView;
 import org.academiadecodigo.tropadelete.views.MainView;
+import org.academiadecodigo.tropadelete.views.RegisterView;
 
 public class ChatClient extends ApplicationAdapter implements InputProcessor {
 
     private LoginView login;
     private LoginView view;
     private ConnectionHandler server;
+    private RegisterView registerView;
 
     @Override
     public void create() {
         view = new LoginView();
-
+        registerView = new RegisterView();
         //server = new ConnectionHandler(new MainView());
         //view.setConnectionHandler(server);
-        view.create();
+        //view.create();
+        registerView.create();
         //server.start();
 
     }
 
     @Override
     public void render() {
-        view.render();
+        registerView.render();
 
     }
 
     @Override
     public void dispose() {
-        view.dispose();
+        registerView.dispose();
     }
 
     @Override
